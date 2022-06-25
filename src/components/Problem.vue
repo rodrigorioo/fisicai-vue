bc00daaedd5cf48a2be0e5a26a309b15e1f28d3f
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2022-06-25 10:03:19 -0300

    Poder resolver un problema con datos Cambios en maquetado

--- src/components/Problem.vue
+++ src/components/Problem.vue
@@ -24,7 +24,7 @@
                     </v-col>
 
                     <v-col cols="12" class="text-right">
-                        <v-btn elevation="2" color="error" dark large
+                        <v-btn elevation="2" color="success" dark large
                             :disabled="button_disabled"
                             @click="solveProblem">
                             RESOLVER
@@ -36,70 +36,108 @@
 
             <v-col cols="12"
 
-                v-if="Object.keys(solution).length > 0">
+                v-if="data.length > 0">
                 <v-card class="mx-auto">
 
                     <v-card-title>Solución</v-card-title>
 
                     <v-card-text>
                         <v-row>
-                            <v-col cols="12" sm="6">
 
-                                <v-list dense>
-                                    <v-subheader>Datos solicitados</v-subheader>
-                                    <v-list-item-group color="primary">
-                                        <v-list-item active-class=""
-                                            v-for="(requestedData, iRequestedData) in solution.requested"
-                                            :key="iRequestedData">
-                                            <v-list-item-content>
-                                                <v-list-item-title>
-                                                    {{ capitalizeWord(requestedData) }}
-                                                </v-list-item-title>
-                                            </v-list-item-content>
-                                        </v-list-item>
-                                    </v-list-item-group>
-                                </v-list>
+                            <v-col cols="12" sm="6" class="pr-10">
+                                <v-row>
 
-                                <v-list dense>
-                                    <v-subheader>Datos obtenidos</v-subheader>
-                                    <v-list-item-group color="primary">
-                                        <v-list-item active-class=""
-                                            v-for="(data, iData) in solution.data"
-                                            :key="iData">
-                                            <v-list-item-content>
-                                                <v-list-item-title>
-                                                    {{ capitalizeWord(data.name) }}: {{ data.value }} {{ data.unit }}
-                                                </v-list-item-title>
-                                            </v-list-item-content>
-                                        </v-list-item>
-                                    </v-list-item-group>
-                                </v-list>
+                                    <v-col cols="12">
+                                        <h4>Datos solicitados</h4>
+                                    </v-col>
+
+                                    <v-col cols="12">
+                                        <v-row>
+                                            <v-col cols="12"
+                                                   v-for="(requestedData, iRequestedData) in requested"
+                                                   :key="iRequestedData">
+                                                <v-text-field
+                                                    @change="modifyRequestedData(iRequestedData, $event.target.value)"
+                                                    :value="requestedData"></v-text-field>
+                                            </v-col>
+                                        </v-row>
+                                    </v-col>
+
+                                    <v-col cols="12">
+                                        <h4>Datos obtenidos</h4>
+                                    </v-col>
+
+                                    <v-col cols="12">
+                                        <v-row>
+                                            <v-col cols="12"
+                                                   v-for="(data, iData) in data"
+                                                   :key="iData">
+                                                <v-row>
+                                                    <v-col cols="12" sm="5">
+                                                        <v-text-field
+                                                            @change="modifyData(iData, 'value', $event.target.value)"
+                                                            :label="capitalizeWord(data.name)"
+                                                            :value="data.value"></v-text-field>
+                                                    </v-col>
+
+                                                    <v-col cols="12" sm="5">
+                                                        <v-text-field label="Unidad"
+                                                            @change="modifyData(iData, 'unit', $event.target.value)"
+                                                            :value="data.unit"></v-text-field>
+                                                    </v-col>
+
+                                                    <v-col cols="12" sm="2">
+                                                        <v-btn class="mx-2" fab dark small color="red"
+                                                               @click="deleteData(iData)">
+                                                            <v-icon dark>mdi-delete</v-icon>
+                                                        </v-btn>
+                                                    </v-col>
+                                                </v-row>
+                                            </v-col>
+                                        </v-row>
+                                    </v-col>
+                                </v-row>
                             </v-col>
 
                             <v-col cols="12" sm="6">
 
-                                <v-list dense>
-                                    <v-subheader>Solución de los datos solicitados</v-subheader>
-                                    <v-list-item-group color="primary">
-                                        <v-list-item active-class=""
-                                            v-for="(resolutionData, iResolutionData) in solution.resolution"
-                                            :key="iResolutionData">
-                                            <v-list-item-content>
-                                                <v-list-item-title>
-                                                    {{ capitalizeWord(resolutionData.name) }}: {{ resolutionData.value }} {{ resolutionData.unit }}
-                                                </v-list-item-title>
-                                            </v-list-item-content>
-                                        </v-list-item>
-                                    </v-list-item-group>
-                                </v-list>
+                                <v-row>
+                                    <v-col cols="12">
+                                        <v-list dense>
+                                            <v-subheader>Solución de los datos solicitados</v-subheader>
+                                            <v-list-item-group color="primary">
+                                                <v-list-item active-class=""
+                                                             v-for="(resolutionData, iResolutionData) in resolution"
+                                                             :key="iResolutionData">
+                                                    <v-list-item-content>
+                                                        <v-list-item-title>
+                                                            {{ capitalizeWord(resolutionData.name) }}: {{ resolutionData.value }} {{ resolutionData.unit }}
+                                                        </v-list-item-title>
+                                                    </v-list-item-content>
+                                                </v-list-item>
+                                            </v-list-item-group>
+                                        </v-list>
+                                    </v-col>
+
+                                    <v-col cols="12">
+                                        <v-btn elevation="2" color="success" dark large
+                                               :disabled="button_disabled"
+                                               @click="resolveProblem">
+                                            RESOLVER CON DATOS
+                                            <v-icon right dark>mdi-send</v-icon>
+                                        </v-btn>
+                                    </v-col>
+                                </v-row>
+
                             </v-col>
+
                         </v-row>
                     </v-card-text>
                 </v-card>
             </v-col>
 
             <v-col cols="12" class="text-center"
-                v-if="Object.keys(solution).length === 0 && button_disabled">
+                v-if="data.length === 0 && button_disabled">
                 <v-progress-circular
                     :size="50"
                     color="primary"
@@ -118,7 +156,9 @@ export default {
 
     data: () => ({
         problem: "",
-        solution: {},
+        data: [],
+        requested: [],
+        resolution: [],
 
         error_message: "",
         show_error_message: false,
@@ -133,7 +173,9 @@ export default {
             // Clear vars
             this.button_disabled = true;
 
-            this.solution = {};
+            this.data = [];
+            this.requested = [];
+            this.resolution = [];
 
             this.show_error_message = false;
             this.error_message = "";
@@ -149,7 +191,52 @@ export default {
             })
                 .then( (response) => {
 
-                    this.solution = response.data;
+                    this.data = response.data.data;
+                    this.requested = response.data.requested;
+                    this.resolution = response.data.resolution;
+
+                })
+                .catch( (errResponse) => {
+
+                    // Show error
+                    this.show_error_message = true;
+                    this.error_message = errResponse.response.data.message;
+
+                    // Reset problem text
+                    this.problem = "";
+
+                }).finally( () => {
+                    this.button_disabled = false;
+            });
+        },
+
+        resolveProblem () {
+
+            // Clear vars
+            this.button_disabled = true;
+
+            this.resolution = [];
+
+            this.show_error_message = false;
+            this.error_message = "";
+
+            // Process request
+            axios.post(`${process.env.VUE_APP_API_URL}solve-problem`, {
+                resolution: {
+                    data: this.data,
+                    requested: this.requested,
+                },
+            }, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then( (response) => {
+
+                    this.data = response.data.data;
+                    this.requested = response.data.requested;
+                    this.resolution = response.data.resolution;
 
                 })
                 .catch( (errResponse) => {
@@ -168,7 +255,19 @@ export default {
 
         capitalizeWord (word) {
             return word.charAt(0).toUpperCase() + word.slice(1);
-        }
+        },
+
+        modifyRequestedData (iRequestedData, value) {
+            this.requested[iRequestedData] = value;
+        },
+
+        modifyData (iData, type, value) {
+            this.data[iData][type] = value;
+        },
+
+        deleteData (iData) {
+            this.data.splice(iData, 1);
+        },
     }
 }
 </script>
