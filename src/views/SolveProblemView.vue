a1d5e7a04c2f6022f6e84f0181d0879cb6af1b32
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2023-08-09 11:12:53 -0300

    + Barra de navegación

--- src/views/SolveProblemView.vue
+++ src/views/SolveProblemView.vue
@@ -1,12 +1,45 @@
 <template>
     <v-app>
         <v-app-bar color="primary" app dark>
+
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
             <div class="d-flex align-center">
                 <!-- LOGO -->
             </div>
 
             <v-spacer></v-spacer>
         </v-app-bar>
+
+        <v-navigation-drawer
+            v-model="drawer"
+            absolute
+            bottom
+            temporary>
+            <v-list
+                nav
+                dense>
+                <v-list-item-group
+                    v-model="group"
+                    active-class="deep-purple--text text--accent-4">
+                    <v-list-item>
+                        <v-list-item-title>Foo</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item>
+                        <v-list-item-title>Bar</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item>
+                        <v-list-item-title>Fizz</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item>
+                        <v-list-item-title>Buzz</v-list-item-title>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
 
         <v-main>
             <Problem />
@@ -26,7 +59,14 @@ export default {
     },
 
     data: () => ({
-        //
+        drawer: false,
+        group: null,
     }),
+
+    watch: {
+        group () {
+            this.drawer = false
+        },
+    },
 };
 </script>
